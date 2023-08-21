014597368eead7a3a20229c4598f4e7ab48cc19b
martin <martin.skvarla11@gmail.com>
2023-08-21 07:55:23 +0200

    changing speed of the ball

--- ball.py
+++ ball.py
@@ -11,32 +11,33 @@ class Ball(turtle.Turtle):
         self.goto(position)
         self.vector = None
         self.ball_current_movement = self.ball_movement_north_east
+        self.ball_speed = 5
 
     def ball_movement_north_east(self):
         self.vector = 'Up'
-        new_x = self.xcor() + 5
-        new_y = self.ycor() + 5
+        new_x = self.xcor() + self.ball_speed
+        new_y = self.ycor() + self.ball_speed
         self.goto(new_x, new_y)
 
     def ball_movement_north_west(self):
 
         self.vector = 'Up'
-        new_x = self.xcor() - 5
-        new_y = self.ycor() + 5
+        new_x = self.xcor() - self.ball_speed
+        new_y = self.ycor() + self.ball_speed
         self.goto(new_x, new_y)
 
     def ball_movement_south_east(self):
 
         self.vector = 'Down'
-        new_x = self.xcor() + 5
-        new_y = self.ycor() - 5
+        new_x = self.xcor() + self.ball_speed
+        new_y = self.ycor() - self.ball_speed
         self.goto(new_x, new_y)
 
     def ball_movement_south_west(self):
 
         self.vector = 'Down'
-        new_x = self.xcor() - 5
-        new_y = self.ycor() - 5
+        new_x = self.xcor() - self.ball_speed
+        new_y = self.ycor() - self.ball_speed
         self.goto(new_x, new_y)
 
 
